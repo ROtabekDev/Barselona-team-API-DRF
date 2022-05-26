28bc8a83955804c779309fd0bcf85962c06cd6dc
ROtabekDev <otabekrahmonovtest@gmail.com>
2022-05-26 08:18:01 +0500

    APIView

--- players/views.py
+++ players/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
+from turtle import title
+from django.forms import model_to_dict
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import generics
+
 from .models import Players
 from .serializers import PlayersSerializer
 
-class PlayersAPIView(generics.ListAPIView):
-    queryset = Players.objects.all()
-    serializer_class = PlayersSerializer
+class PlayersAPIView(APIView):
+    def get(self, request):
+        lst = Players.objects.all().values()
+        return Response({'posts': list(lst)})
+
+    def post(self, request):
+        post_new = Players.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
+        )
+        return Response({'post': model_to_dict(post_new)})
+
+
+# class PlayersAPIView(generics.ListAPIView):
+#     queryset = Players.objects.all()
+#     serializer_class = PlayersSerializer
 
     
