3071170afae2380bbb56b8ef80b2c788e1517b20
ROtabekDev <otabekrahmonovtest@gmail.com>
2022-05-28 09:05:58 +0500

    Методы save(), create() и update() класса Serializer

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -7,4 +7,5 @@ from players.views import PlayersAPIView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/playerslist/',PlayersAPIView.as_view()),
+    path('api/v1/playerslist/<int:pk>/',PlayersAPIView.as_view()),
 ]
--- players/views.py
+++ players/views.py
@@ -10,22 +10,39 @@ from .serializers import PlayersSerializer
 class PlayersAPIView(APIView):
    
     def get(self, request):
-        p = Players.objects.all().values()
+        p = Players.objects.all()
         return Response({'posts': PlayersSerializer(p, many=True).data})
 
 
     def post(self, request):
         serializer = PlayersSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        serializer.save()
 
+        return Response({'post': serializer.data})
 
-        post_new = Players.objects.create(
-            title=request.data['title'],
-            content=request.data['content'],
-            cat_id=request.data['cat_id']
-        )
-        return Response({'post': PlayersSerializer(post_new).data})
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get("pk", None)
+        if not pk:
+            return Response({"error": "Method PUT not allowed"})
+        
+        try:
+            instance = Players.objects.get(pk=pk)
+        except:
+            return Response({"error": "Object does not exists"})
 
+        serializer = PlayersSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response({"post": serializer.data})
+
+    def delete(self, request, *args, **kwargs):
+        pk = kwargs.get("pk", None)
+        if not pk:
+            return Response({"error": "Method DELETE not allowed"})
+
+        return Response({"post": "delete post" + str(pk)})
 
 # class PlayersAPIView(generics.ListAPIView):
 #     queryset = Players.objects.all()
