114fb6c43290379fad2ffc2a9dd7e2f6508ada9a
ROtabekDev <otabekrahmonovtest@gmail.com>
2022-05-30 19:06:46 +0500

    Представления UpdateAPIView и RetrieveUpdateDestroyAPIView

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -2,10 +2,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from players.views import PlayersAPIList, PlayersAPIView
+from players.views import PlayersAPIList, PlayersAPIUpdate, PlayersAPIDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/playerslist/',PlayersAPIList.as_view()),
-    path('api/v1/playerslist/<int:pk>/',PlayersAPIList.as_view()),
+    path('api/v1/playerslist/<int:pk>/', PlayersAPIUpdate.as_view()),
+    path('api/v1/playersdetail/<int:pk>/', PlayersAPIDetailView.as_view()),
 ]
--- players/views.py
+++ players/views.py
@@ -12,42 +12,53 @@ class PlayersAPIList(generics.ListCreateAPIView):
     queryset = Players.objects.all()
     serializer_class = PlayersSerializer
 
-class PlayersAPIView(APIView):
+
+class PlayersAPIUpdate(generics.UpdateAPIView):
+    queryset = Players.objects.all()
+    serializer_class = PlayersSerializer
+
+
+class PlayersAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Players.objects.all()
+    serializer_class = PlayersSerializer
+    
+
+# class PlayersAPIView(APIView):
    
-    def get(self, request):
-        p = Players.objects.all()
-        return Response({'posts': PlayersSerializer(p, many=True).data})
+#     def get(self, request):
+#         p = Players.objects.all()
+#         return Response({'posts': PlayersSerializer(p, many=True).data})
 
 
-    def post(self, request):
-        serializer = PlayersSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+#     def post(self, request):
+#         serializer = PlayersSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
 
-        return Response({'post': serializer.data})
+#         return Response({'post': serializer.data})
 
-    def put(self, request, *args, **kwargs):
-        pk = kwargs.get("pk", None)
-        if not pk:
-            return Response({"error": "Method PUT not allowed"})
+#     def put(self, request, *args, **kwargs):
+#         pk = kwargs.get("pk", None)
+#         if not pk:
+#             return Response({"error": "Method PUT not allowed"})
         
-        try:
-            instance = Players.objects.get(pk=pk)
-        except:
-            return Response({"error": "Object does not exists"})
+#         try:
+#             instance = Players.objects.get(pk=pk)
+#         except:
+#             return Response({"error": "Object does not exists"})
 
-        serializer = PlayersSerializer(data=request.data, instance=instance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+#         serializer = PlayersSerializer(data=request.data, instance=instance)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
 
-        return Response({"post": serializer.data})
+#         return Response({"post": serializer.data})
 
-    def delete(self, request, *args, **kwargs):
-        pk = kwargs.get("pk", None)
-        if not pk:
-            return Response({"error": "Method DELETE not allowed"})
+#     def delete(self, request, *args, **kwargs):
+#         pk = kwargs.get("pk", None)
+#         if not pk:
+#             return Response({"error": "Method DELETE not allowed"})
 
-        return Response({"post": "delete post" + str(pk)})
+#         return Response({"post": "delete post" + str(pk)})
 
 # class PlayersAPIView(generics.ListAPIView):
 #     queryset = Players.objects.all()
